9e9a2590451bf6144e36f4ee8bba698f1ecb526b
Samuel-Kowatschek <samkow@gmx.at>
2019-06-05 20:47:52 +0200

    changed some formatting and deleted unused lines

--- list_all_students.c
+++ list_all_students.c
@@ -3,23 +3,27 @@
 #define len 64
 
 int main(int argc, char const *argv[]) {
+  
   int age;
-
-  FILE* fd=fopen("student.dat","r");
-  int id;
-
-  fseek(fd,0,SEEK_SET);
-  fseek(fd,sizeof(int),SEEK_SET);
-  printf("%10s %-32s %-32s %-3s\n", "Id","First Name","Last Name","Age");
-  int count = fread(&id,sizeof(int),1,fd);
   char first_name[len];
   char last_name[len];
-  while (count > 0) {
-    fread(first_name,sizeof(first_name),1,fd);
-    fread(last_name,sizeof(last_name),1,fd);
-    fread(&age,sizeof(int),1,fd);
-    count = fread(&id,sizeof(int),1,fd);
-    printf("%010d %-32s %-32s %-3d\n", id, first_name, last_name, age);
+  int id;
+  FILE* fd = fopen("student.dat", "r");
+  //go to the first student
+
+  fseek(fd, 0, SEEK_SET);
+  fseek(fd, sizeof(int), SEEK_SET);
+  
+  printf("%10s %-20s %-20s %10s\n", "Id", "First Name", "Last Name", "Age");
+
+  int count = fread(&id, sizeof(int), 1, fd);
+  while(count > 0)
+  {
+    fread(first_name, sizeof(first_name), 1, fd);
+    fread(last_name, sizeof(last_name), 1, fd);
+    fread(&age, sizeof(age), 1, fd);
+    printf("%-10d %-20s %-20s %10d\n", id, first_name, last_name, age );
+    count = fread(&id, sizeof(int), 1, fd);
   }
   fclose(fd);
   return 0;
