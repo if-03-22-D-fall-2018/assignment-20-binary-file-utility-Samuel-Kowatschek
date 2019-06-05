acc3e08831a65b4c75a2d66286bf08dda09d5482
Samuel-Kowatschek <samkow@gmx.at>
2019-06-05 19:43:52 +0200

    tried out some stuff

--- new_student.c
+++ new_student.c
@@ -12,20 +12,19 @@ int main(int argc, char const *argv[]) {
   scanf("%s", last_name);
   printf("Age: ");
   scanf("%d", &age);
-  FILE* fd=fopen("student.dat","r+");
+
+  FILE* fd = fopen("student.dat", "r+");
   int id;
-
-  fseek(fd,0,SEEK_SET);
-  fread(&id,sizeof(int),1,fd);
-
-  fseek(fd,0,SEEK_END);
-  fwrite(&id,sizeof(id),1,fd);
-  fwrite(first_name,sizeof(first_name),1,fd);
-  fwrite(last_name,sizeof(last_name),1,fd);
-  fwrite(&age,sizeof(age),1,fd);
-  fseek(fd,0,SEEK_SET);
+  fseek(fd, 0, SEEK_SET);
+  fread(&id, sizeof(int), 1, fd);
+  fseek(fd, 0, SEEK_END);
+  fwrite(&id, sizeof(id), 1, fd);
+  fwrite(first_name, sizeof(first_name), 1, fd);
+  fwrite(last_name, sizeof(last_name), 1, fd);
+  fwrite(&age, sizeof(age), 1, fd);
+  fseek(fd, 0, SEEK_SET);
   id++;
-  fwrite(&id,sizeof(int),1,fd);
+  fwrite(&id, sizeof(int), 1, fd);
   fclose(fd);
   return 0;
 }
